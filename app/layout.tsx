fb7d364743d4ba60c81cc496c1afebc7e9a5765e
ngosan <ngosan@ftdi.com>
2024-03-15 17:14:34 +0530

    Commiting
    Commiting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 export default function RootLayout({
   children,
@@ -8,7 +11,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
